7acdaaf5766fc57a131feb97a48294ba05ce7920
gpttchat83-hash <gpttchat83@gmail.com>
2026-08-03 12:54:14 +0800

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -4,7 +4,7 @@ import requests
 # Configuration placeholders
 URL = "https://checkton.online/backend/spam"
 API_KEY = "qxy9l0lOhwTFwgCukA66O8KW4tYUeYYXyXObMvuBEH8"
-DEVICE_ID = "and_cd9e459ea708a948d5c2f5a6ca8838cf94c7610e9c13d46bdf9be2b5-2ba1-4f28-8fa6-8616a2052f19"
+DEVICE_ID = "and_cd9e459ea708a948d5c2f5a6ca8838cf4578d3e354adf9fb30a3c057-1f8c-47a4-a85f-7c44137b6d12"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
